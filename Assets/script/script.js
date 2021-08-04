1b7929022268abe38629cb4351e4107bb75ef9ff
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-08-04 15:13:01 -0400

    updated the verification of the initial length input to make sure that it's a number. Also added a bunch of comments to help whoever is looking to follow along with the logic

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -6,11 +6,12 @@ function writePassword() {
 
 var passLength = prompt("How long would you like your password to be? Please select a number larger than 7 and less than 129.");
 
-// verification that the length of the password meets the requirements of minimum 8 characters, maximum of 128 characters.
+// verification that the length of the password meets the requirements of minimum 8 characters, maximum of 128 characters. Also verifying that the text entered in the input is a number.
 
-if (passLength < 7 || passLength > 128) {
-  while (passLength < 7 || passLength > 128) {
-    passLength = prompt("Please re-enter, making sure the number is greater than 7 and less than 129");
+
+if ((passLength < 8 || passLength > 128) || isNaN(passLength) === true) {
+  while ((passLength < 8 || passLength > 128) || isNaN(passLength) === true) {
+    passLength = prompt("Try again. Make sure that you enter a number that is greater than 7 and less that 129");
   }
 }
 
@@ -59,15 +60,14 @@ if (passSpecial === true) {
 var generatedPassword = []; // created placeholder for array where password will be generated.
 
 
-//creating a while loop to run through password verification. The for loop is going to create the password and if it doesn't pass the verification at the end then it's going to run through the for loop again until it does pass
+//creating a while loop to run through password verification. The for loop is going to create the password and if it doesn't pass the verification at the end then it's going to run through the for loop again until it does pass. Setting verification as false to start so that it will enter the while loop on the first go round.
 
 var verification = false;
 
 while (verification == false) {
 
-// loop that runs through as many times as was requested in the first prompt
+for (i=0; i < passLength; i++) {  // loop that runs through as many times as was requested in the first prompt
 
-for (i=0; i < passLength; i++) {
 
 // creating random numbers, one to select which array the generator is going to pull from, and the next to determine which character is going to be pulled from the array
 
@@ -89,7 +89,8 @@ generatedPassword.push(selectedCharacter);
 
 passwordText = generatedPassword.join("");
 
-}
+} // this is where the for loop ends
+
 
 // this is the space after the for loop, but still within the while loop. Here the password will be verified.
 
@@ -97,6 +98,8 @@ var upperRegex = new RegExp("^(?=.*[A-Z])");
 var lowerRegex = new RegExp("^(?=.*[a-z])");
 var numberRegex = new RegExp("^(?=.*[0-9])");
 var specialRegex = new RegExp("^(?=.*[!@#$%^&*])");
+
+// Here we check that if a character was initially selected then it appears in the password at least once.
 
 if (passUpper === true) {
   var upperTest = upperRegex.test(passwordText);
@@ -114,6 +117,8 @@ if (passSpecial === true) {
   var specialTest = specialRegex.test(passwordText);
 }
 
+// if any of the character types were selected but didn't show up then one of the four variables will return false and the loop will try again.
+
 if (upperTest === false || lowerTest === false || numberTest === false || specialTest === false) {
   verification = false;
   i = -1;
@@ -122,13 +127,13 @@ if (upperTest === false || lowerTest === false || numberTest === false || specia
 
 }
 else {
-  verification = true;
+  verification = true; // If verification passes then the verification variable will be set to true and the program will exit the over-arching while loop.
 }
 
 
 }
 
-// locates the element that has the ID of "password" and replaces the text with the password string
+// locates the element that has the ID of "password" and replaces the text with the final password string.
 
 document.getElementById("password").textContent = passwordText;
 
