45efdf898d0b092b3b0e914da8ac87aa548a7b25
kjphelps90 <87020287+kjphelps90@users.noreply.github.com>
2021-08-04 12:57:27 -0400

    was able to create verification comparing my output against RegExp, if each requested character type isn't in the password then it fails verification. If verification is failed then i is set to -1 so that it will be a 0 once the for loop is hit again. The array setup to gather up the password, along with the password text is removed as well. So everything gets wiped awaway and the program starts again until it delivers a password that meets all the requirements.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,13 @@
 // Assignment Code
 // var generateBtn = document.querySelector("#generate");
 
+
 function writePassword() {
 
 var passLength = prompt("How long would you like your password to be? Please select a number larger than 7 and less than 129.");
 
-if (passLength < 8 || passLength > 128) {
-  while (passLength < 8 || passLength > 128) {
+if (passLength < 7 || passLength > 128) {
+  while (passLength < 7 || passLength > 128) {
     passLength = prompt("Please re-enter, making sure the number is greater than 7 and less than 129");
   }
 }
@@ -19,7 +20,7 @@ var passSpecial = confirm("Would you like your password to include special chara
 var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+var specialArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
 var combinedArray = [];
 
@@ -42,6 +43,12 @@ if (passSpecial === true) {
 // created placeholder for array where password will be generated.
 var generatedPassword = [];
 
+//creating a while loop to run through password verification. The for loop is going to create the password and if it doesn't pass the verification at the end then it's going to run through the for loop again until it does pass
+
+var verification = false;
+
+while (verification == false) {
+
 // loop that runs through as many times as was requested in the first prompt
 
 for (i=0; i < passLength; i++) {
@@ -61,15 +68,52 @@ var selectedCharacter = selectedArray[characterSelector];
 
 generatedPassword.push(selectedCharacter);
 
-// Converting the array to string
+
+// If everything goes well then the array gets converted to string
 
 passwordText = generatedPassword.join("");
 
-// going to have to write something up that will ensure that each of the selected character types is present in the generated password.
+}
+
+// this is the space after the for loop, but still within the while loop. Here the password will be verified 
+
+var upperRegex = new RegExp("^(?=.*[A-Z])");
+var lowerRegex = new RegExp("^(?=.*[a-z])");
+var numberRegex = new RegExp("^(?=.*[0-9])");
+var specialRegex = new RegExp("^(?=.*[!@#$%^&*])");
+
+if (passUpper === true) {
+  var upperTest = upperRegex.test(passwordText);
+}
+
+if (passLower === true) {
+  var lowerTest = lowerRegex.test(passwordText);
+}
+
+if (passNumber === true) {
+  var numberTest = numberRegex.test(passwordText);
+}
+
+if (passSpecial === true) {
+  var specialTest = specialRegex.test(passwordText);
+}
+
+if (upperTest === false || lowerTest === false || numberTest === false || specialTest === false) {
+  verification = false;
+  i = -1;
+  passwordText = "";
+  generatedPassword = [];
+
+}
+else {
+  verification = true;
+}
+
+
 
 }
 
-// Possibly create a separate function that will post it to the box
+// locates the element that has the ID of "password" and replaces the text with the password string
 
 document.getElementById("password").textContent = passwordText;
 
